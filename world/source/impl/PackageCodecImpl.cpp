71ad18e45edda498ba4030d751b51d676c359988
admin <xxx@xx.xom>
2025-02-08 09:51:58 +0800

    Initial commit

--- world/source/impl/PackageCodecImpl.cpp
+++ world/source/impl/PackageCodecImpl.cpp
@@ -37,21 +37,18 @@ awaitable<void> UPackageCodecImpl::EncodeT(FPackage *pkg) {
 }
 
 awaitable<void> UPackageCodecImpl::DecodeT(FPackage *pkg) {
-    FPackage::FHeader header{};
-    memset(&header, 0, sizeof(header));
-
-    if (const auto len = co_await async_read(mConn->GetSocket(), asio::buffer(&header, FPackage::kHeaderSize)); len == 0) {
+    if (const auto len = co_await async_read(mConn->GetSocket(), asio::buffer(&pkg->header, FPackage::kHeaderSize)); len == 0) {
         spdlog::warn("{} - Read package header length equal zero", __FUNCTION__);
         pkg->Invalid();
         co_return;
     }
 
-    pkg->header.magic = ntohl(header.magic);
-    pkg->header.version = ntohl(header.version);
-    pkg->header.method = static_cast<ECodecMethod>(ntohs(static_cast<uint16_t>(header.method)));
+    pkg->header.magic = ntohl(pkg->header.magic);
+    pkg->header.version = ntohl(pkg->header.version);
+    pkg->header.method = static_cast<ECodecMethod>(ntohs(static_cast<uint16_t>(pkg->header.method)));
 
-    pkg->header.id = ntohl(header.id);
-    pkg->header.length = ntohll(header.length);
+    pkg->header.id = ntohl(pkg->header.id);
+    pkg->header.length = ntohll(pkg->header.length);
 
     if (pkg->header.length == 0)
         co_return;
