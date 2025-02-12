cea828b6b1a793af0c528c0461dc6b7adef5ca32
admin <xxx@xx.xom>
2025-02-12 15:33:38 +0800

    Initial commit

--- world/include/system/database/db_table.h
+++ world/include/system/database/db_table.h
@@ -1,5 +1,27 @@
 #pragma once
 
-class IDBTable {
+#include "../../common.h"
 
+#include <mysqlx/xdevapi.h>
+
+
+class BASE_API IDBTable {
+
+public:
+    virtual ~IDBTable() = default;
+
+    [[nodiscard]] virtual const char* GetTableName() const = 0;
+
+    [[nodiscard]] virtual bool ComparePrimaryKey(mysqlx::Row &row) const = 0;
+
+    virtual mysqlx::RowResult Query(mysqlx::Table &table) = 0;
+    virtual mysqlx::RowResult Query(mysqlx::Schema &schema) = 0;
+
+    virtual void Read(mysqlx::Row &row) = 0;
+
+    virtual void Write(mysqlx::Table &table) = 0;
+    virtual void Write(mysqlx::Schema &schema) = 0;
+
+    virtual void Remove(mysqlx::Table &table) = 0;
+    virtual void Remove(mysqlx::Schema &schema) = 0;
 };
