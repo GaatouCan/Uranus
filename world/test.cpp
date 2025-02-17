1aa6e353236ff35a081dcff68aa04edef8bf1d4e
admin <xxx@xx.xom>
2025-02-17 20:12:20 +0800

    Initial commit

---
+++ world/test.cpp
@@ -0,0 +1,28 @@
+#include <spdlog/spdlog.h>
+#include <mysqlx/xdevapi.h>
+#include <iostream>
+
+int main() {
+
+    try {
+        mysqlx::Session sess("localhost", 33060, "root", "12345678");
+        auto schema = sess.getSchema("demo");
+
+        if (auto table = schema.getTable("avatar"); table.existsInDatabase()) {
+            table.insert("pid", "index", "expired_time", "activated", "in_used")
+                    .values(112002, 1, 21143224, true, true)
+                    .execute();
+
+            auto result = table.select().execute();
+
+            if (auto row = result.fetchOne(); !row.isNull()) {
+                std::cout << row[0] << row[1] << row[2] << row[3] << row[4] << std::endl;
+            }
+        }
+
+    } catch (std::exception &e) {
+        spdlog::error("{}", e.what());
+    }
+
+    return 0;
+}
