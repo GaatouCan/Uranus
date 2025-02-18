780f95c2ece3840adff9d0a3d503a03a1fb041f6
Astria <1752651025@qq.com>
2025-02-19 00:10:27 +0800

    Initial commit

--- world/world.cpp
+++ world/world.cpp
@@ -2,18 +2,19 @@
 
 #include "game_world.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+    constexpr auto SERVER_DLL_FILENAME = "server.dll";
+#else
+    constexpr auto SERVER_DLL_FILENAME = "libserver.so";
+#endif
+
 
 int main(int argc, char *argv[]) {
     spdlog::set_level(spdlog::level::info);
 
     const auto world = new GameWorld();
 
-#if defined(_WIN32) || defined(_WIN64)
-    world->Init("server.dll");
-#else
-    world->Init("libserver.so");
-#endif
-
+    world->Init(SERVER_DLL_FILENAME);
     world->Run();
 
     delete world;
