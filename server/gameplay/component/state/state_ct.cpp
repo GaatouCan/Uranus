5fc38832e66ff164e1dd281d9266d83621ec8c34
admin <xxx@xx.xom>
2025-02-22 10:44:30 +0800

    Initial commit

--- server/gameplay/component/state/state_ct.cpp
+++ server/gameplay/component/state/state_ct.cpp
@@ -3,10 +3,21 @@
 //
 
 #include "state_ct.h"
+#include "../../../player/component_module.h"
+
+#include "utils.h"
 
 StateCT::StateCT(IComponentContext *ctx)
     : IPlayerComponent(ctx) {
 }
 
 StateCT::~StateCT() {
+}
+
+ISerializer * StateCT::Serialize_State(bool &bExpired) const {
+    WRITE_PARAM(State, mState)
+}
+
+void StateCT::Deserialize_State(Deserializer &ds) {
+    READ_PARAM(ds, mState)
 }
