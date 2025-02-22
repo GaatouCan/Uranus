db4c1e177ae7dcff3ad2d56396097f5e045a737d
admin <xxx@xx.xom>
2025-02-22 17:34:26 +0800

    Initial commit

--- server/gameplay/component/state/state_ct.h
+++ server/gameplay/component/state/state_ct.h
@@ -23,6 +23,6 @@ public:
     ISerializer *Serialize_State(bool &bExpired) const;
     void Deserialize_State(Deserializer &ds);
 
-    int32_t GetLevel() const;
-    int64_t GetExp() const;
+    [[nodiscard]] int32_t GetLevel() const;
+    [[nodiscard]] int64_t GetExp() const;
 };
