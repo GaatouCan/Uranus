04af6803227845b50c53380f0a50bd87bafd3566
admin <xxx@xx.xom>
2025-02-22 10:34:16 +0800

    Initial commit

--- struct/sql/state.sql
+++ struct/sql/state.sql
@@ -2,7 +2,7 @@ CREATE TABLE `state`
 (
     `pid` BIGINT NOT NULL COMMENT '玩家ID',
     `level` INT NOT NULL DEFAULT 0 COMMENT '等级',
-    `exprience` BIGINT NOT NULL DEFAULT 0 COMMENT '经验值',
+    `experience` BIGINT NOT NULL DEFAULT 0 COMMENT '经验值',
     `last_login_time` BIGINT NOT NULL DEFAULT 0 COMMENT '最后登陆时间',
     `last_logout_time` BIGINT NOT NULL DEFAULT 0 COMMENT '最后登出时间',
     PRIMARY KEY (`pid`)
