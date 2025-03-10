9311e83535f41e1eeae63684c77593f8b45c711d
admin <xxx@xx.xom>
2025-03-10 12:07:35 +0800

    Initial commit

---
+++ world/include/poolable.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "common.h"
+
+
+class BASE_API IPoolable {
+
+    friend class IRecycler;
+    IRecycler* handle_;
+
+protected:
+
+    explicit IPoolable(IRecycler* handle);
+    virtual ~IPoolable();
+
+    virtual void initial() = 0;
+    virtual void reset() = 0;
+
+public:
+    IPoolable() = delete;
+
+    DISABLE_COPY_MOVE(IPoolable)
+
+    virtual bool copyFrom(IPoolable *other) = 0;
+
+    virtual void recycle();
+
+    [[nodiscard]] virtual bool available() const = 0;
+};
---
+++ world/source/poolable.cpp
@@ -0,0 +1,13 @@
+#include "../include/poolable.h"
+#include "../include/recycler.h"
+
+
+IPoolable::IPoolable(IRecycler *handle)
+    : handle_(handle) {
+}
+
+IPoolable::~IPoolable() {
+}
+
+void IPoolable::recycle() {
+}
