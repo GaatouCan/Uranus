2ccbee586d3ab25379c9d49f0d8229673a763622
admin <xxx@xx.xom>
2025-03-18 19:51:41 +0800

    Initial commit

--- world/include/object.h
+++ world/include/object.h
@@ -7,9 +7,13 @@
 
 class BASE_API UObject {
 
-    std::vector<UObject*> children_;
+    UObject *parent_ = nullptr;
+    std::vector<UObject *> children_;
 
 public:
+    UObject();
+    explicit UObject(UObject *parent);
+
     virtual ~UObject();
 
 protected:
@@ -19,5 +23,23 @@ protected:
         const auto res = new T(std::forward<Args>(args)...);
         children_.push_back(res);
         return res;
+    }
+
+    template<class T, class... Args>
+    requires std::derived_from<T, UObject>
+    T *createSubObject(Args && ... args) {
+        const auto res = new T(this, std::forward<Args>(args)...);
+        children_.push_back(res);
+        return res;
+    }
+
+    [[nodiscard]] UObject *getParent() const;
+
+    template<class T>
+    requires std::derived_from<T, UObject>
+    T *getParentT() const {
+        if (parent_ != nullptr)
+            return dynamic_cast<T *>(parent_);
+        return nullptr;
     }
 };
--- world/source/object.cpp
+++ world/source/object.cpp
@@ -1,7 +1,18 @@
 #include "../include/object.h"
 
+UObject::UObject() {
+}
+
+UObject::UObject(UObject *parent)
+    : parent_(parent) {
+}
+
 UObject::~UObject() {
     for (const auto &child : children_) {
         delete child;
     }
+}
+
+UObject * UObject::getParent() const {
+    return parent_;
 }
